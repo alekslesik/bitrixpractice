acefca98612875a93bc175e35762187259c0ef39
alekslesik <alekslesik@gmail.com>
2022-02-28 21:03:07 +0300

    fill step.php fill ru/step.php

--- modules/kasianov.totop/lang/ru/install/step.php
+++ modules/kasianov.totop/lang/ru/install/step.php
@@ -1,1 +1,5 @@
 <?php
+
+$MESS["FALBAR_TOTOP_STEP_BEFORE"]        = "Модуль";
+$MESS["FALBAR_TOTOP_STEP_AFTER"]         = "установлен";
+$MESS["FALBAR_TOTOP_STEP_SUBMIT_BACK"] = "Вернуться в список";
